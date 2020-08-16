a7a777c73a385c9dd68a33bdafd3a86d4100ba61
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-08-16 13:36:52 +0200

    create API module

--- joplin-importfolder.py
+++ joplin-importfolder.py
@@ -2,15 +2,10 @@
 
 # -*- coding: utf-8 -*-
 
-import time
 import click
 import sys
 import os
-import json
 import time
-import requests
-import mimetypes
-import base64
 import joplinapi
 
 @click.command()
@@ -42,21 +37,16 @@ def Main(path, destination, token):
         print("Path does not exist")
         sys.exit(1)
 
-    global JOPLIN_NOTEBOOK_NAME
-    global JOPLIN_NOTEBOOK_ID
-    global JOPLIN_TOKEN
-    global JOPLIN_ENDPOINT
-    JOPLIN_ENDPOINT = "http://localhost:41184"
-    JOPLIN_NOTEBOOK_NAME = destination
-    JOPLIN_TOKEN = token
-    JOPLIN_NOTEBOOK_ID = JoplinGetNotebookID(JOPLIN_NOTEBOOK_NAME)
-    if JOPLIN_NOTEBOOK_ID == "" or JOPLIN_NOTEBOOK_ID == "err":
+    joplinapi.SetEndpoint("http://localhost:41184",token)
+    notebook_id = joplinapi.GetNotebookID(destination)
+
+    if notebook_id == False:
         print("Notebook not found")
         sys.exit(1)
 
-    WatchFolder(path)
+    WatchFolder(path, notebook_id)
 
-def WatchFolder(path):
+def WatchFolder(path, notebook_id):
     files = dict()
     while 1:
         # Add files and process
@@ -65,8 +55,8 @@ def WatchFolder(path):
                 print("Added: " + file)
                 files[file] = os.path.getsize(os.path.join(path, file))
             elif os.path.getsize(os.path.join(path, file)) == files[file]:
-                print("Process: " + file)
-                if JoplinUpload(os.path.join(path, file), JOPLIN_NOTEBOOK_ID):
+                print("Upload to Joplin: " + file)
+                if joplinapi.CreateNoteWithFile(os.path.join(path, file), notebook_id, {".md"}):
                     try:
                         os.remove(os.path.join(path, file))
                         files.pop(file)
@@ -87,104 +77,6 @@ def WatchFolder(path):
                 files.pop(file)
 
         time.sleep(1)
-
-
-def JoplinGetNotebookID(notebook_name):
-    """ Find the ID of the destination folder 
-    adapted logic from jhf2442 on Joplin forum
-    https://discourse.joplin.cozic.net/t/import-txt-files/692
-    """
-    notebook_id = ""
-
-    try:
-        res = requests.get(JOPLIN_ENDPOINT + "/folders?token=" + JOPLIN_TOKEN)
-        folders = res.json()
-
-        for folder in folders:
-            if folder.get("title") == notebook_name:
-                notebook_id = folder.get("id")
-        if notebook_id == "":
-            for folder in folders:
-                if "children" in folder:
-                    for child in folder.get("children"):
-                        if child.get("title") == notebook_name:
-                            notebook_id = child.get("id")
-    except requests.ConnectionError as e:
-        print("Connection Error - Is Joplin Running?")
-    except Exception as e:
-        print("Error - on get joplin notebook id")
-
-    return notebook_id
-
-def JoplinEncodeFile(filename, datatype):
-    file = open(filename, "rb") 
-    encoded = base64.b64encode(file.read())
-    file.close()
-    img = f"data:{datatype};base64,{encoded.decode()}"
-    return img
-
-def JoplinUpload(file, notebook_id):
-    print("Upload to Joplin: " + file)
-
-    datatype = mimetypes.guess_type(file)[0]
-    print(datatype)
-
-    if datatype is None:
-        datatype = "None"
-
-    if datatype == "text/plain" or os.path.splitext(os.path.basename(file))[1] in {".md"}:
-        with open(file, "r") as txt:
-            text = txt.read()
-        values = JoplinCreateJson(os.path.basename(file), notebook_id, text)
-    elif datatype[:5] == "image":
-        data = JoplinEncodeFile(file, datatype)
-        values = JoplinCreateJson(os.path.basename(file), notebook_id, "", data)
-    else:
-        resource = JoplinCreateResource(file)
-        if resource == False:
-            print("Resource creation error")
-            return False
-        
-        file_link = "[" + os.path.basename(file) + "](:/" + resource['id'] + ")"
-        values = JoplinCreateJson(os.path.basename(file), notebook_id, file_link)
-    
-    requests_return = requests.post(
-        JOPLIN_ENDPOINT + "/notes?token=" + JOPLIN_TOKEN, data=values)
-    if requests_return.status_code == 200:
-        print("Upload OK")
-        return True
-    else:
-        print(requests_return.text)
-        print("Upload ERROR")
-        print("")
-        print("")
-        print(values)
-        return False
-
-def JoplinCreateResource(file):
-    filename = os.path.basename(file)
-    title = os.path.splitext(os.path.basename(file))[0]
-    f = open(file, "rb") 
-    files = {
-        "data": (json.dumps(file), f),
-        "props": (None, f'{{"title":"{title}", "filename":"{filename}"}}'),
-    }
-    response = requests.post(JOPLIN_ENDPOINT + "/resources?token=" + JOPLIN_TOKEN, files=files)
-    f.close()
-    if response.status_code != 200:
-        return False
-    else:
-        return response.json()
-
-def JoplinCreateJson(title, notebook_id, body, file=None):
-    if file is None:
-        return '{{ "title": {}, "parent_id": "{}", "body": {} }}'.format(
-            json.dumps(title), notebook_id, json.dumps(body)
-        )
-    else:
-        return '{{ "title": "{}", "parent_id": "{}", "body": {}, "image_data_url": "{}" }}'.format(
-            title, notebook_id, json.dumps(body), file
-        )
 
 if __name__ == "__main__":
     Main()
---
+++ joplinapi.py
@@ -0,0 +1,119 @@
+import requests
+import mimetypes
+import os
+import json
+import base64
+import sys
+import mimetypes
+
+def Test(file, notebook_id):
+    GetEndpoint()
+
+def SetEndpoint(endpoint, token):
+    global JOPLIN_ENDPOINT
+    global JOPLINAPI_TOKEN
+    JOPLIN_ENDPOINT = endpoint
+    JOPLINAPI_TOKEN = token
+
+def GetEndpoint():
+    endpoint = {"endpoint":JOPLIN_ENDPOINT, "token": JOPLINAPI_TOKEN}
+    return endpoint
+
+def GetNotebookID(notebook_name):
+    """ Find the ID of the destination folder 
+    adapted logic from jhf2442 on Joplin forum
+    https://discourse.joplin.cozic.net/t/import-txt-files/692
+    """
+    notebook_id = ""
+    joplin = GetEndpoint()
+
+    try:
+        res = requests.get(joplin['endpoint'] + "/folders?token=" + joplin['token'])
+        folders = res.json()
+
+        for folder in folders:
+            if folder.get("title") == notebook_name:
+                notebook_id = folder.get("id")
+        if notebook_id == "":
+            for folder in folders:
+                if "children" in folder:
+                    for child in folder.get("children"):
+                        if child.get("title") == notebook_name:
+                            notebook_id = child.get("id")
+    except requests.ConnectionError as e:
+        print("Connection Error - Is Joplin Running?")
+    except Exception as e:
+        print("Error - on get joplin notebook id")
+
+    if notebook_id == "" or notebook_id == "err":
+        return False
+    else:
+        return notebook_id
+
+def CreateNoteWithFile(file, notebook_id, ext_as_text = None):
+    joplin = GetEndpoint()
+
+    datatype = mimetypes.guess_type(file)[0]
+
+    if datatype is None:
+        datatype = "None"
+
+    if datatype == "text/plain" or os.path.splitext(os.path.basename(file))[1] in ext_as_text:
+    #if datatype == "text/plain" or os.path.splitext(os.path.basename(file))[1] in {".md"}:
+        with open(file, "r") as txt:
+            text = txt.read()
+        values = CreateJsonForNote(os.path.basename(file), notebook_id, text)
+    elif datatype[:5] == "image":
+        data = EncodeResourceFile(file, datatype)
+        values = CreateJsonForNote(os.path.basename(file), notebook_id, "", data)
+    else:
+        resource = CreateResource(file)
+        if resource == False:
+            print("Resource creation error")
+            return False
+        
+        file_link = "[" + os.path.basename(file) + "](:/" + resource['id'] + ")"
+        values = CreateJsonForNote(os.path.basename(file), notebook_id, file_link)
+    
+    requests_return = requests.post(
+        joplin['endpoint'] + "/notes?token=" + joplin['token'] , data=values)
+    if requests_return.status_code == 200:
+        return True
+    else:
+        #print(requests_return.text)
+        print("Note creation ERROR")
+        return False
+
+def CreateResource(file):
+    joplin = GetEndpoint()
+
+    filename = os.path.basename(file)
+    title = os.path.splitext(os.path.basename(file))[0]
+    f = open(file, "rb") 
+    files = {
+        "data": (json.dumps(file), f),
+        "props": (None, f'{{"title":"{title}", "filename":"{filename}"}}'),
+    }
+    response = requests.post(joplin['endpoint'] + "/resources?token=" + joplin['token'], files=files)
+    f.close()
+    if response.status_code != 200:
+        return False
+    else:
+        return response.json()
+
+def CreateJsonForNote(title, notebook_id, body, file=None):
+    if file is None:
+        return '{{ "title": {}, "parent_id": "{}", "body": {} }}'.format(
+            json.dumps(title), notebook_id, json.dumps(body)
+        )
+    else:
+        return '{{ "title": "{}", "parent_id": "{}", "body": {}, "image_data_url": "{}" }}'.format(
+            title, notebook_id, json.dumps(body), file
+        )
+
+def EncodeResourceFile(filename, datatype):
+    file = open(filename, "rb") 
+    encoded = base64.b64encode(file.read())
+    file.close()
+    img = f"data:{datatype};base64,{encoded.decode()}"
+    return img
